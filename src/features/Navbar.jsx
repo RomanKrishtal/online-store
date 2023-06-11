6e291c08af4b8ae91c0cb2bcb2543a69a9e0aa55
Роман <rom.krishtal98@gmail.com>
2023-06-11 12:08:03 +0200

    footer added

---
+++ src/features/Navbar.jsx
@@ -0,0 +1,26 @@
+const Navbar = () => {
+    return(
+        <>
+        <div>
+        <img></img>
+        <p>Deliver to</p>
+        <input type="text" />
+        <button>
+            <img></img>
+        </button>
+        <span>Language</span>
+        <span>Returns & orders</span>
+        <span>Cart</span>
+        </div>
+        <div>
+            <ul>
+                <li><a></a></li>
+                <li><a></a></li>
+                <li><a></a></li>
+                <li><a></a></li>
+                <li><a></a></li>
+            </ul>
+        </div>
+        </>
+    )
+}
