338b37e82833f4ed17e4bddca871afee38a7a393
Роман <rom.krishtal98@gmail.com>
2023-06-13 22:18:36 +0200

    password moved

--- src/features/Content.jsx
+++ src/features/Content.jsx
@@ -2,17 +2,6 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 function Content() {
-    const options = {
-        method: 'GET',
-        url: 'https://shopify-fast-scraper.p.rapidapi.com/product',
-        params: {
-          url: 'https://shop.flipperzero.one/collections/all/products/flipper-zero'
-        },
-        headers: {
-          'X-RapidAPI-Key': '29b81b1274msh393d32133b6b6e1p1bf747jsn5a615c5de96a',
-          'X-RapidAPI-Host': 'shopify-fast-scraper.p.rapidapi.com'
-        }
-      };
 
     useEffect(() => {
         const fetchData = async () => {
